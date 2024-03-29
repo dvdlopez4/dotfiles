a0c6353484cd73c2a1d0c71ec5584f34211b6808
David Lopez <david.lopez@warnerpacific.com>
2024-03-29 09:19:15 -0700

    Fixed tsserver on_attach

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -139,7 +139,6 @@ return {
                         local lspconfig = require("lspconfig")
                         lspconfig.tsserver.setup {
                             on_attach = function(client, bufnr)
-                                print("hello tsserver, again...")
                                 client.server_capabilities.documentFormattingProvider = false
                                 client.server_capabilities.documentRangeFormattingProvider = false
                             end,
@@ -177,7 +176,6 @@ return {
 
             })
 
-
             vim.diagnostic.config({
                 virtual_text = true
             })
@@ -195,22 +193,6 @@ return {
             -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
             vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
             vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-            -- Option 2: nvim lsp as LSP client
-            -- Tell the server the capability of foldingRange,
-            -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.foldingRange = {
-                dynamicRegistration = false,
-                lineFoldingOnly = true
-            }
-            local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-            for _, ls in ipairs(language_servers) do
-                require('lspconfig')[ls].setup({
-                    capabilities = capabilities
-                    -- you can add other fields for setting up lsp server in this table
-                })
-            end
-            require('ufo').setup()
 
             require('ufo').setup({
                 provider_selector = function(bufnr, filetype, buftype)
