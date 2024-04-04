71d42274228724da37266f502a30cce9a09a8be8
David Lopez <david.lopez@warnerpacific.com>
2024-04-04 10:21:25 -0700

    Explicitly enable tree-sitter highlighting

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -12,6 +12,14 @@ return {
                 -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
                 auto_install = true,
 
+                indent = {
+                    enable = true
+                },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = { "markdown" }
+                },
+
                 incremental_selection = {
                     enable = true,
                     keymaps = {
