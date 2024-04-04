7ed327a6cabec2c00c56d086912d7046842095e9
David Lopez <david.lopez@warnerpacific.com>
2024-04-04 10:55:56 -0700

    Replace tree with oil

--- .config/nvim/lua/plugins/navigation.lua
+++ .config/nvim/lua/plugins/navigation.lua
@@ -77,17 +77,12 @@ return {
         end
     },
     {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-        keys = {
-            { "<leader>ee", "<cmd>Neotree toggle focus<CR>", desc = "Toggle Neotree" },
-            { "<leader>ef", "<cmd>Neotree focus<CR>",        desc = "Focus on Neotree" }
-        }
-    },
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("oil").setup()
+        end
+    }
 }
