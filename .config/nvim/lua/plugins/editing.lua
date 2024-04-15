703b68c9f29df97d9a0b0e7349385905fcf579f3
David Lopez <dvdlopez4@gmail.com>
2024-04-14 22:39:55 -0700

    Using daemons for linting and formatting

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -34,12 +34,16 @@ return {
     { 'nvim-treesitter/playground' },
     {
         'nvimtools/none-ls.nvim',
+        dependencies = {
+            "nvimtools/none-ls-extras.nvim",
+        },
         config = function()
             local null_ls = require("null-ls")
 
             -- register any number of sources simultaneously
             local sources = {
-                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.prettierd,
+                require("none-ls.diagnostics.eslint_d")
             }
 
             null_ls.setup({ sources = sources })
