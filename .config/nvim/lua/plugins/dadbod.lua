8b59a7e917aaa80238a0a8d6099658f233ca6766
David Lopez <dvdlopez4@gmail.com>
2024-05-21 07:47:40 -0700

    Diffget key changes and improved lazy loading

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -5,10 +5,9 @@ return {
         "kristijanhusak/vim-dadbod-completion"
     },
     config = function()
-        vim.keymap.set("n", "<leader>db", function()
-            vim.cmd('DBUIToggle')
-        end, { desc = "Open database UI" })
-
         vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
-    end
+    end,
+    keys = {
+        { "<leader>db", "<cmd>DBUIToggle<CR>", desc = "Open database UI" },
+    },
 }
