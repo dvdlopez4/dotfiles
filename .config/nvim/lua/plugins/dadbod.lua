a5f9fe6b1352c3a814923bc1b6f4029ac7619e1d
David Lopez <dvdlopez4@gmail.com>
2024-06-05 09:07:33 -0700

    Dadbod restructure

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -1,13 +1,33 @@
 return {
-    "tpope/vim-dadbod",
-    dependencies = {
+    {
+        "tpope/vim-dadbod",
+    },
+    {
         "kristijanhusak/vim-dadbod-ui",
-        "kristijanhusak/vim-dadbod-completion"
+        dependencies = { "tpope/vim-dadbod" },
+        config = function()
+            vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
+        end,
+        keys = {
+            { "<leader>db", ":tabe DBUIToggle<CR>", desc = "Open database UI" },
+        }
     },
-    config = function()
-        vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
-    end,
-    keys = {
-        { "<leader>db", "<cmd>DBUIToggle<CR>", desc = "Open database UI" },
-    },
+    {
+        "kristijanhusak/vim-dadbod-completion",
+        dependencies = { "tpope/vim-dadbod" },
+        ft = { "sql", "mysql", "plsql" },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("kide_vim_dadbod_completion", { clear = true }),
+                pattern = { "sql", "mysql", "plsql" },
+                callback = function(_)
+                    require("cmp").setup.buffer({
+                        sources = {
+                            { name = "vim-dadbod-completion" }
+                        }
+                    })
+                end,
+            })
+        end
+    }
 }
