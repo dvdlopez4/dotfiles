e37faf797abad4390986469e02b49496b3d89663
David Lopez <dvdlopez4@gmail.com>
2024-06-16 09:53:52 -0700

    Adding copilot keymaps

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -4,12 +4,19 @@ return {
         "CopilotC-Nvim/CopilotChat.nvim",
         branch = "canary",
         dependencies = {
-            { "github/copilot.vim" }, -- or github/copilot.vim
-            { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+            { "github/copilot.vim" },    -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
         opts = {
             -- See Configuration section for rest
         },
-        -- See Commands section for default commands if you want to lazy load on them
+        keys = {
+            {
+                "<leader>cc", "<cmd>CopilotChatToggle<CR>", desc = "Start Copilot Chat Toggle"
+            },
+            {
+                "<leader>ce", "<cmd>CopilotChatExplain<CR>", desc = "Start Copilot Explain this"
+            },
+        }
     }
 }
