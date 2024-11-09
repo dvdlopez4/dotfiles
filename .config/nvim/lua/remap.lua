03b12799a6eb9379b2048d8de92f576585aa6cfb
David Lopez <dvdlopez4@gmail.com>
2024-11-09 01:50:31 -0800

    format highlighted lines

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -21,6 +21,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end)
+vim.keymap.set('v', '<leader>f', vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -28,7 +29,6 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>tn", function()
     vim.cmd("tabnew")
 end, { desc = "Create a new tab" })
