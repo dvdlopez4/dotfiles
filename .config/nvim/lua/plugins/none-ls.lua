b5d280ad64ef42585460211d0b1e9fe88cd6e91c
David Lopez <dvdlopez4@gmail.com>
2024-11-09 01:52:28 -0800

    remove formatting on save

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -16,24 +16,8 @@ return {
             })
         }
 
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         null_ls.setup({
             sources = sources,
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({
-                        group = augroup,
-                        buffer = bufnr,
-                    })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format({ bufnr = bufnr })
-                        end,
-                    })
-                end
-            end
         })
     end
 }
