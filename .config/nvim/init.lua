54af96d39240a8c11b61a61f18e1441a48fed388
David Lopez <dvdlopez4@gmail.com>
2026-01-07 16:02:29 -0600

    updates?

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
@@ -40,51 +40,53 @@ autocmd("TextYankPost", {
     end,
 })
 
--- autocmd("LspAttach", {
---     group = ThePrimeagenGroup,
---     callback = function(e)
---         local opts = { buffer = e.buf, silent = true }
---         vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
---         vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
---         vim.keymap.set("n", "K", function()
---             vim.lsp.buf.hover()
---         end, opts)
---         vim.keymap.set("n", "<leader>vws", function()
---             vim.lsp.buf.workspace_symbol()
---         end, opts)
---         vim.keymap.set("n", "<leader>vd", function()
---             vim.diagnostic.open_float()
---         end, opts)
---         vim.keymap.set("n", "<leader>vca", function()
---             vim.lsp.buf.code_action()
---         end, opts)
---         vim.keymap.set("n", "<leader>vrn", function()
---             vim.lsp.buf.rename()
---         end, opts)
---         vim.keymap.set("i", "<C-h>", function()
---             vim.lsp.buf.signature_help()
---         end, opts)
---         vim.keymap.set("n", "[d", function()
---             vim.diagnostic.goto_next()
---         end, opts)
---         vim.keymap.set("n", "]d", function()
---             vim.diagnostic.goto_prev()
---         end, opts)
+autocmd("LspAttach", {
+    group = ThePrimeagenGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf, silent = true }
+        vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, opts)
+        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set("n", "<leader>vws", function()
+            vim.lsp.buf.workspace_symbol()
+        end, opts)
+        vim.keymap.set("n", "<leader>vd", function()
+            vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set("n", "<leader>vca", function()
+            vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set("n", "<leader>vrn", function()
+            vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set("i", "<C-h>", function()
+            vim.lsp.buf.signature_help()
+        end, opts)
+        vim.keymap.set("n", "[d", function()
+            vim.diagnostic.jump({ count = -1, float = true })
+        end, opts)
+        vim.keymap.set("n", "]d", function()
+            vim.diagnostic.jump({ count = 1, float = true })
+        end, opts)
+        opts.desc = "Show buffer diagnostics"
+        vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
---         -- local client = vim.lsp.get_client_by_id(e.data.client_id)
---         -- if client and client.server_capabilities.documentHighlightProvider then
---         --     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
---         --         buffer = e.buf,
---         --         callback = vim.lsp.buf.document_highlight,
---         --     })
+        local client = vim.lsp.get_client_by_id(e.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                buffer = e.buf,
+                callback = vim.lsp.buf.document_highlight,
+            })
 
---         --     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
---         --         buffer = e.buf,
---         --         callback = vim.lsp.buf.clear_references,
---         --     })
---         -- end
---     end,
--- })
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                buffer = e.buf,
+                callback = vim.lsp.buf.clear_references,
+            })
+        end
+    end,
+})
 
 local severity = vim.diagnostic.severity
 
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,17 +3,25 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         { "antosha417/nvim-lsp-file-operations", config = true },
-        { "folke/lazydev.nvim", opts = {} },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/love2d/library" },
+                },
+            },
+        },
+        config = function()
+            -- import cmp-nvim-lsp plugin
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+            -- used to enable autocompletion (assign to every lsp server config)
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
+        end,
     },
-    config = function()
-        -- import cmp-nvim-lsp plugin
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-        -- used to enable autocompletion (assign to every lsp server config)
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-
-        vim.lsp.config("*", {
-            capabilities = capabilities,
-        })
-    end,
 }
